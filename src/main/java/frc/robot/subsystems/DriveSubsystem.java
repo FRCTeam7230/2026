52f7a9d647ccafec54aa0b2bea7a9f01bf3ab0b6
Graham Hoggan <grahambentleyhoggan@gmail.com>
2026-02-02 18:30:36 -0800

    Added Auto Megatag method

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -330,6 +330,53 @@ public void updateOdometry() {
         pose);
   }
 
+  public void AddMegatagVision()
+  {
+    Optional<Alliance> ally = DriverStation.getAlliance();
+    if (ally.isPresent()) {
+        if (ally.get() == Alliance.Red) {
+          int[] validIDs = {2,5,8,9,10,11};
+          LimelightHelpers.SetFiducialIDFiltersOverride("limelight", validIDs);
+        }
+        if (ally.get() == Alliance.Blue) {
+          int[] validIDs = {18,21,24,25,26,27};
+          LimelightHelpers.SetFiducialIDFiltersOverride("limelight", validIDs);
+        }
+    }
+    else {
+      int[] validIDs = {2,5,8,9,10,11,18,21,24,25,26,27};
+      LimelightHelpers.SetFiducialIDFiltersOverride("limelight", validIDs);
+    }
+      currentPose = m_odometry.update(
+        Rotation2d.fromDegrees(getFieldAngle()),
+        new SwerveModulePosition[] {
+            m_frontLeft.getPosition(),
+            m_frontRight.getPosition(),
+            m_rearLeft.getPosition(),
+            m_rearRight.getPosition()
+        });
+    boolean doRejectUpdate = false;
+
+      LimelightHelpers.SetRobotOrientation("limelight", m_odometry.getEstimatedPosition().getRotation().getDegrees(), 0, 0, 0, 0, 0);
+      LimelightHelpers.PoseEstimate mt2 = LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2("limelight");
+      if(Math.abs(m_gyro.getRate()) > 720) // if our angular velocity is greater than 720 degrees per second, ignore vision updates
+      {
+        doRejectUpdate = true;
+      }
+      if(mt2.tagCount == 0)
+      {
+        doRejectUpdate = true;
+      }
+      if(!doRejectUpdate)
+      {
+        m_odometry.setVisionMeasurementStdDevs(VecBuilder.fill(.7,.7,9999999));
+        m_odometry.addVisionMeasurement(
+            mt2.pose,
+            mt2.timestampSeconds);
+      }
+  
+  }
+
   /**
    * Method to drive the robot using joystick info.
    *
