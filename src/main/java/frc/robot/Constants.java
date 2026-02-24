40ab8ca7a855249c51649c91fbc433f825da9fb2
DaMooseMan <musa.k.ghani@gmail.com>
2026-02-23 18:30:49 -0800

    custom gradient, intake, align, idle, publishers, periodic stuff also

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -262,6 +262,30 @@ public final class Constants {
     
     public static final Color kNiceYellow = new Color("#DEC95D"); 
     public static final Color kBrightNiceYellow = new Color("#FFE666");
+    public static final Color kGreen = new Color("#32CD32");
+    public static final Color kOrange = new Color("#FF8D54");
+    public static final Color kPurple = Color.kMediumPurple;
+
+    // custom gradient
+    public static final int khalfPercentageFromBottom = 1/8;
+    public static final double krepeatTimes = 1; // how many times the pattern should repeat across the strip, if applicable
+    public static final double kshootingMovingFrequency = 0.5; // how many times per second the pattern should move, if applicable
+    public static final double kidleMovingFrequency = 0.25; // how many times per second the pattern should move, if applicable
+    public static final double kintakeMovingFrequency = 1; // how many times per second the pattern should move, if applicable
+
+    // purple hsv
+    public static final int kPurpleH = 260;
+    public static final int kPurpleS = 49;
+    public static final int kPurpleV = 86;
+    // yellow hsv
+    public static final int kYellowH = 50;
+    public static final int kYellowS = 60;
+    public static final int kYellowV = 100;
+    // orange hsv
+    public static final int kOrangeH = 20;
+    public static final int kOrangeS = 67;
+    public static final int kOrangeV = 100;
+
 
     
   }
--- src/main/java/frc/robot/subsystems/LEDSubsystem.java
+++ src/main/java/frc/robot/subsystems/LEDSubsystem.java
@@ -4,12 +4,18 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.networktables.BooleanPublisher;
+import edu.wpi.first.networktables.DoublePublisher;
+import edu.wpi.first.networktables.IntegerPublisher;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.units.Units;
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.AddressableLEDBufferView;
 import edu.wpi.first.wpilibj.LEDPattern;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.util.Color;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
@@ -19,54 +25,67 @@ public class LEDSubsystem extends SubsystemBase {
   AddressableLEDBuffer m_LEDBuffer;
   AddressableLEDBufferView m_bottom;
   AddressableLEDBufferView m_top;
+  boolean isOverriden;
+  Timer robotTimer; 
+  int currentState;
+
+  BooleanPublisher isOverridenPublisher = NetworkTableInstance.getDefault().getBooleanTopic("LEDSubsystem/isOverriden").publish();
+  IntegerPublisher currentStatePublisher = NetworkTableInstance.getDefault().getIntegerTopic("LEDSubsystem/CurrentState").publish();
+  DoublePublisher robotTimerPublisher = NetworkTableInstance.getDefault().getDoubleTopic("LEDSubsystem/RobotTimer").publish();
+
+  
 
   public LEDSubsystem() {
     m_LED = new AddressableLED(Constants.LEDConstants.kPort);
     m_LEDBuffer = new AddressableLEDBuffer(Constants.LEDConstants.kLEDLength);
     m_bottom = m_LEDBuffer.createView(Constants.LEDConstants.kBottomStartIndex, Constants.LEDConstants.kBottomEndIndex);
     m_top = m_LEDBuffer.createView(Constants.LEDConstants.kTopStartIndex, Constants.LEDConstants.kTopEndIndex);
+    robotTimer = new Timer();
+    isOverriden = false;
+    currentState = 0; // 0 is idle
 
     m_LED.setLength(m_LEDBuffer.getLength());
     m_LED.setData(m_LEDBuffer);
     m_LED.start();
+    robotTimer.start();
   }
 
+  public void solidColorAll(Color c) {
+    LEDPattern colorPattern = LEDPattern.solid(c);
+    colorPattern.applyTo(m_LEDBuffer);
+    m_LED.setData(m_LEDBuffer);
+  }
+
+  //current state = 1
   public void solidPurpleAll() {
-    LEDPattern purplePattern = LEDPattern.solid(Color.kMediumPurple);
-    purplePattern .applyTo(m_LEDBuffer);
-    m_LED.setData(m_LEDBuffer);
+    currentState = 1;
+    solidColorAll(Color.kMediumPurple);// #9370DB
   }
 
+  //current state = 2
   public void solidYellowAll() {
-    LEDPattern yellowPattern = LEDPattern.solid(Constants.LEDConstants.kNiceYellow);// #DEC95D - contrasts medium purple
-    yellowPattern.applyTo(m_LEDBuffer);
-    m_LED.setData(m_LEDBuffer);
+    currentState = 2;
+    solidColorAll(Constants.LEDConstants.kNiceYellow);// #DEC95D - contrasts medium purple
   }
 
-  // int
-  // when put in periodic might want to add a delay?
+  //current state = 3
+  public void solidGreenAll() {
+    currentState = 3;
+    solidColorAll(Constants.LEDConstants.kGreen);
+  }
 
-  public void transitionBlinkingColorSmoothAll() { // could change later if want to use mask and make breath (brightness really fast but as a funtion of percentage)
+  //current state = 4
+  public void transitionBlinkingColorSmoothAll(int hubState) { // could change later if want to use mask and make breath (brightness really fast but as a funtion of percentage)
     //is valid
-    int currentPhase = FieldManagementPublisher.getHubState();
+    int currentPhase = hubState;
     if (!((currentPhase == 2) || (currentPhase == 3))) {
       return;
     }
+    currentState = 4;
     // yes valid
-    double percentage = 1 - (FieldManagementPublisher.timeLeftInTransition() / 3); //start transition: 0; end transition: 1
+    double percentage = 1 - (FieldManagementPublisher.timeLeftInTransition(currentPhase) / 3); //start transition: 0; end transition: 1
     if (percentage > 1) {
       return;
-    }
-    //select which is starting color based on type of transition
-    Color first;
-    Color second;
-    if (currentPhase == 2) { // testing: change to 3 if backwards
-      first = Constants.LEDConstants.kNiceYellow;
-      second = Color.kMediumPurple;
-    }
-    else {
-      first = Color.kMediumPurple;
-      second = Constants.LEDConstants.kNiceYellow;
     }
     //blinking or solid yelpurp
     Color actual;
@@ -74,22 +93,96 @@ public class LEDSubsystem extends SubsystemBase {
       actual = Color.kBlack;
     }
     else {
+      //select which is starting color based on type of transition
+      Color first;
+      Color second;
+      if (currentPhase == 2) { // testing: change to 3 if backwards
+        first = Constants.LEDConstants.kNiceYellow;
+        second = Color.kMediumPurple;
+      }
+      else {
+        first = Color.kMediumPurple;
+        second = Constants.LEDConstants.kNiceYellow;
+      }
       actual = Color.lerpRGB(first, second, percentage);
     }
     //set pattern
-    LEDPattern yelpurpPattern = LEDPattern.solid(actual);
-    yelpurpPattern.applyTo(m_LEDBuffer);
-    m_LED.setData(m_LEDBuffer);
+    solidColorAll(actual);
   }
   
   /** LED pattern runs during Auto(gradient, breathe, and scroll) */
+
+  //current state = 5
   public void autoPattern() {
+    currentState = 5;
     LEDPattern base = LEDPattern.gradient(LEDPattern.GradientType.kContinuous, Color.kMediumPurple, Constants.LEDConstants.kNiceYellow);
     LEDPattern DCbreathe = base.breathe(Units.Seconds.of(2));
     LEDPattern pattern = base.scrollAtRelativeSpeed(Units.Hertz.of(0.5));
     LEDPattern maskedthing = DCbreathe.mask(pattern);
     maskedthing.applyTo(m_LEDBuffer);
     m_LED.setData(m_LEDBuffer);
+  }
+
+  public void setIsOverriden(boolean over) {
+    isOverriden = over;
+  }
+  public boolean getIsOverriden() {
+    return isOverriden;
+  }
+
+  //current state = 6
+  public void shootingPattern() { 
+    currentState = 6;
+    setCustomGradientToBuffer(Constants.LEDConstants.kYellowH, Constants.LEDConstants.kYellowS, Constants.LEDConstants.kYellowV, Constants.LEDConstants.kshootingMovingFrequency);
+    m_LED.setData(m_LEDBuffer);
+  }
+
+  // manually sets gradient to buffer
+  public void setCustomGradientToBuffer(int h, int s, int v, double movingFrequency) {
+    double time = robotTimer.get();
+    for (int i=0; i < m_LEDBuffer.getLength(); i++) {
+      int vAfterBrightness = (int)(v * calculateBrightnessPercentage(i, time, movingFrequency));
+      Color colorAfterBrightness = Color.fromHSV(h, s, vAfterBrightness);
+      m_LEDBuffer.setLED(i, colorAfterBrightness);
+    }
+  }
+
+  // calculates brightness of purple based on LEDLength and repeatTimes along the led strip
+  private double calculateBrightnessPercentage(int index, double t, double movingFrequency) {
+    double offset;
+    if (t >= 0) {
+      offset = (movingFrequency * t * Constants.LEDConstants.kLEDLength) % Constants.LEDConstants.kLEDLength;
+    }
+    else {
+      offset = 0;
+    }
+
+    double period = Constants.LEDConstants.kLEDLength/Constants.LEDConstants.krepeatTimes;
+    double a = 1/2 - Constants.LEDConstants.khalfPercentageFromBottom; 
+    double b = (2*Math.PI)/period;
+    double c = 1/2 + Constants.LEDConstants.khalfPercentageFromBottom;
+    return a*Math.cos(b*(index - offset)) + c;
+  }
+
+  public void idlePattern() {
+    currentState = 0;
+    setCustomGradientToBuffer(Constants.LEDConstants.kPurpleH, Constants.LEDConstants.kPurpleS, Constants.LEDConstants.kPurpleV, Constants.LEDConstants.kshootingMovingFrequency);
+    m_LED.setData(m_LEDBuffer);
+  }
+
+  public void intakePattern() {
+    currentState = 7;
+    setCustomGradientToBuffer(Constants.LEDConstants.kOrangeH, Constants.LEDConstants.kOrangeS, Constants.LEDConstants.kOrangeV, Constants.LEDConstants.kintakeMovingFrequency);
+    m_LED.setData(m_LEDBuffer);
+  }
+
+  //commandss
+  public Command whileAlignSolidGreen() { //call with aligntohub alongwith i think
+    return startEnd(()->{setIsOverriden(true); solidGreenAll();}, ()->{setIsOverriden(false);});
+  }
+
+  public Command whileShootingPattern() { //call with shooting trigger is held alongwith i think
+    return startEnd(()->{setIsOverriden(true); shootingPattern();}, ()->{setIsOverriden(false);});
   }
 
   @Override
@@ -101,21 +194,35 @@ public class LEDSubsystem extends SubsystemBase {
    * 2 = transition (active to inactive)
    * 3 = transition (inactive to active)
    */
-    double hubState = FieldManagementPublisher.getHubState();
-    if (hubState == 2 || hubState == 3) {
-      transitionBlinkingColorSmoothAll();
+    if (!isOverriden || currentState == 0) {
+      int hubState = FieldManagementPublisher.getHubState();
+      if (hubState == 2 || hubState == 3) {
+        transitionBlinkingColorSmoothAll(hubState);
+      }
+      if (hubState == 1 && currentState != 2) {
+        solidYellowAll();
+      }
+      if (hubState == 0 && currentState != 1) {
+        solidPurpleAll();
+      }
+      if (hubState == -1 && currentState != 5) {
+        autoPattern();
+      }
     }
-    if (hubState == 1) {
-      solidYellowAll();
+    else if (currentState == 6) {
+      shootingPattern();
     }
-    if (hubState == 0) {
-      solidPurpleAll();
+    else if (currentState == 7) {
+      intakePattern();
+    }
+    else {
+      idlePattern();
     }
     
-    if (hubState == -1) {
-      autoPattern();
-    }
-      
+    isOverridenPublisher.set(isOverriden);
+    currentStatePublisher.set(currentState);
+    robotTimerPublisher.set(robotTimer.get());
+
   }
   
 }
