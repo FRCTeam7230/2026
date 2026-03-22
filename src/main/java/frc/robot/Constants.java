685f2064f4098b1770195a56eee7e7cbaaa74ac8
Alex Danielsen <adh.danielsen@gmail.com>
2026-03-22 11:57:26 -0700

    LA Regional end of qualifying matches

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -150,7 +150,7 @@ public final class Constants {
   public static final class DriveConstants {
     // Driving Parameters - Note that these are not the maximum capable speeds of
     // the robot, rather the allowed maximum speeds
-    public static final double kMaxSpeedMetersPerSecond = 3.6;//was 4.8
+    public static final double kMaxSpeedMetersPerSecond = 4.8;//was 4.8
     public static final double kMaxAngularSpeed = 2 * Math.PI; // radians per second
 
     // Chassis configuration
